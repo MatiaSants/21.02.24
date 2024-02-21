17c80a9c885754cbaab6e59c93370247d19b0a15
Matias <142435857+MatiaSants@users.noreply.github.com>
2024-02-21 12:21:01 -0300

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,43 @@
+const sortear = () => {
+    var nomes = [ "Miguel", "Gabriel", "guilherme", "Meca"];
+
+    let h1Nome = document.getElementById("nome");
+
+    let achouNumero = false;
+    let numeroSorteado;
+
+    while (achouNumero == false) {
+        numeroSorteado = Math.floor(Math.random() * 100);
+        if (numeroSorteado < nomes.length) {
+            achouNumero = true;
+        } 
+    }
+
+    switch (numeroSorteado) {
+        case 0:
+            h1Nome.innerText = nomes[numeroSorteado];
+            break;
+
+        case 1:
+            h1Nome.innerText = nomes[numeroSorteado];
+            break;
+
+        case 2:
+            h1Nome.innerText = nomes[numeroSorteado];
+            break;
+
+        case 3:
+            h1Nome.innerText = nomes[numeroSorteado];
+            break;
+
+        case 4:
+            h1Nome.innerText = nomes[numeroSorteado];
+            break;
+
+            default:
+                h1Nome.innerText = "Ops! Nome não consta na lista";
+                break;
+    }
+    h1Nome.innerText += "o numero sorteado foi: " + numeroSorteado;
+
+}
